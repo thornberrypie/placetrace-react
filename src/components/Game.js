bac3363a046c41ca20e328c138a37ae6f5313049
GraemeT <gdthornber@gmail.com>
2018-12-29 02:48:18 +1300

    Fetch data from restcountries API

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,19 +3,27 @@ import CountryImage from './CountryImage'
 import svgData from '../data/svgCountries.json'
 
 class Game extends Component {
-  render() {
-    return (
-      <section className="game">
-        <h2>Guess the country</h2>
-        <div className="country">{ this.getCountryImage('ae') }</div>
-      </section>
-    )
+
+  constructor() {
+    super();
+    this.state = {
+      countryCode: 'ad',
+      countryName: 'Andorra'
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(response => response.json())
+      .then(data =>
+        console.log(data.length + ' countries')
+      )
   }
 
   getCountryImage(countryCode) {
     return (
       svgData.map((svgData, index) => {
-        if(svgData.id == countryCode) {
+        if(svgData.id === countryCode) {
           return (
             <CountryImage
               key = {index}
@@ -27,6 +35,18 @@ class Game extends Component {
           )
         }
       })
+    )
+  }
+
+  render() {
+    return (
+      <section className="game">
+        <h2>Guess the country</h2>
+        { this.state.data }
+        <div className="country">
+          { this.getCountryImage(this.state.countryCode) }
+        </div>
+      </section>
     )
   }
 }
