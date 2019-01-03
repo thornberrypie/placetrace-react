13dc65f0a7cecc0da139914396ed50e7188b14d8
GraemeT <gdthornber@gmail.com>
2019-01-04 00:49:51 +1300

    Enable region filtering in country menu

--- src/components/CountrySelect.js
+++ src/components/CountrySelect.js
@@ -12,48 +12,42 @@ const Regions = [
 class CountrySelect extends Component {
   constructor() {
     super()
-
     this.state = {
       countrySelectData: []
     }
-
-
   }
 
   componentWillReceiveProps() {
     this.buildSelectMenu();
-
   }
 
-  buildSelectMenu() {
-    const countries = this.props.countries
+  buildSelectMenu(selectedRegion) {
+    const Countries = this.props.countries
     let countrySelectData = [];
-
     // Create new array of objects for select menu
-    Object.keys(countries).forEach(function(key) {
-      var country = countries[key]
+    Object.keys(Countries).forEach(function(key) {
+      let country = Countries[key]
+      // If region is selected allow only that region's countries
+      if(selectedRegion && selectedRegion !== country.region) {
+        return
+      }
       countrySelectData.push({
         value: country.alpha2Code.toLowerCase(),
         label: country.name
       })
-
     })
     this.setState({
       countrySelectData: countrySelectData
     })
   }
 
-  filterCountryMenu(region) {
-    console.log(region)
-  }
-
-  handleCountryChange = event => {
-    console.log(event.value)
+  handleCountryChange = e => {
+    console.log(e.value)
   }
 
   handleRegionClick = e => {
     e.preventDefault();
-    this.filterCountryMenu(e.target.innerText)
+    this.buildSelectMenu(e.target.innerText)
   }
 
   render() {
@@ -74,9 +68,7 @@ class CountrySelect extends Component {
                     key={index}
                     className="button button--filter"
                     onClick={this.handleRegionClick}
-                  >
-                    {region}
-                  </button>
+                  >{region}</button>
                 )
               }, this)}
             </div>
